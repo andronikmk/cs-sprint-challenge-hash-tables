14d1c48125d83d9db0fec8d7293566f0fd696483
andronikmk <andronikmk@gmail.com>
2020-07-10 10:34:16 -0400

    Add ex4

--- hashtables/ex4/ex4.py
+++ hashtables/ex4/ex4.py
@@ -3,6 +3,18 @@ def has_negatives(a):
     YOUR CODE HERE
     """
     # Your code here
+    # instantiate an array for results
+    result = []
+    # instantiate a dictionary
+    dictionary = {}
+    # loop though array of positive and neg numbers
+    for i in a:
+        # only works when i set dict value to something.
+        dictionary[i] = None
+        # if -i is in dictionary and it is i not = to zero
+        if -i in dictionary and i != 0:
+        # append to your results make sure the value is the absolute i.e. abs()
+            result.append(abs(i))
 
     return result
 
