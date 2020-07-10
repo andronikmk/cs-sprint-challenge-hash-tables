45b1837d3c430ae02b75d9707efc8441da361412
andronikmk <andronikmk@gmail.com>
2020-07-10 11:09:58 -0400

    Add ex5

--- hashtables/ex5/ex5.py
+++ hashtables/ex5/ex5.py
@@ -7,7 +7,30 @@ def finder(files, queries):
     YOUR CODE HERE
     """
     # Your code here
+    # instantiate a results array
+    result = []
+    # instantiate a dictionary
+    dictionary = {}
+    # loop though the files
+    for f in files:
+        # split string according to "/"
+        uncouple = f.split('/')
+        # print(uncouple)
+        # name of file is last element
+        name_of_file = uncouple[-1]
+        # if file name is not in dictionary set as empty array 
+        if name_of_file not in dictionary:
+            dictionary[name_of_file] = []
+        # else append dict values
+        dictionary[name_of_file].append(f)
 
+    # loop thorugh queries
+    for query in queries:
+        # if query in dictionary
+        if query in dictionary:
+            # extend to results dictionary[q]
+            result.extend(dictionary[query])
+    
     return result
 
 
